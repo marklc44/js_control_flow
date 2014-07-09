5778cb1f581e75fdcc52d8df60c0991e55fddb9e
Mark Centoni <marklc44@gmail.com>
2014-07-08 20:29:40 -0700

    added switch statement to grade.js

--- grade.js
+++ grade.js
@@ -1,4 +1,5 @@
-function grader(grade) {
+function grader(score) {
+	
 	if (score >= 90) {
 		return "A";
 	} else if (score >= 80 && score < 90) {
@@ -12,6 +13,29 @@ function grader(grade) {
 	}
 }
 
+function graderSwitch(score) {
+
+	switch (true) {
+		case (score >= 90):
+			return "A";
+			break;
+		case (score >= 80 && score < 90):
+			return "B";
+			break;
+		case (score >= 70 && score < 80):
+			return "C";
+			break;
+		case (score >= 60 && score < 70):
+			return "D";
+			break;
+		default:
+			return "F";
+			break;
+	}
+}
+
+
 console.log(grader(90));
+console.log(graderSwitch(82));
 
 
